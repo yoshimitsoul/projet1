6680e447690a0c4edf320e7e836d18940fef8078
yoshimitsoul <openclasspierre@gmail.com>
2019-04-10 17:45:10 +0200

    including println and \n for creating space

--- src/projet1/sdz1.java
+++ src/projet1/sdz1.java
@@ -3,7 +3,9 @@ package projet1;
 public class sdz1 {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World !");
+		System.out.print("Hello World !\n");
+		System.out.println("my name is");
+		System.out.println("\n Yoshimitsoul");
 		// TODO Auto-generated method stub
 
 	}
