e86dcc2dbc62e97afd8c8eca57f1e2c237eb2125
yoshimitsoul <openclasspierre@gmail.com>
2019-04-12 09:43:38 +0200

    some calculs

--- src/projet1/sdz1.java
+++ src/projet1/sdz1.java
@@ -5,9 +5,12 @@ public class sdz1 {
 	public static void main(String[] args) {
 		System.out.print("Hello World !\n");
 		System.out.println("my name is");
-		System.out.println("\n Yoshimitsoul");
+		System.out.println("\n Yoshimitsoul \n");
 		// TODO Auto-generated method stub
-
+		
+		double nbre1 = 10, nbre2 = 3;
+		int resultat = (int)(nbre1 / nbre2);
+		System.out.println("Le résultat est = " + resultat);
 	}
 
 }
